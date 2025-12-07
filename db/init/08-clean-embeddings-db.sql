0ed123dda7411080822b542babaa14b33a0d4040
AuronCrow10 <122154238+AuronCrow10@users.noreply.github.com>
2025-12-07 10:39:10 +0100

    db_fix

---
+++ db/init/08-clean-embeddings-db.sql
@@ -0,0 +1,6 @@
+\connect embeddings_db;
+
+DELETE FROM client_usage;
+DELETE FROM clients;
+DELETE FROM page_chunks_large;
+DELETE FROM page_chunks_small;
---
+++ db/init/09-clean-chatbot-db.sql
@@ -0,0 +1,15 @@
+\connect chatbot
+
+
+DELETE FROM "Bot";
+DELETE FROM "BotChannel";
+DELETE FROM "Conversation";
+DELETE FROM "EmailVerificationToken";
+DELETE FROM "Message";
+DELETE FROM "MetaConnectSession";
+DELETE FROM "OpenAIUsage";
+DELETE FROM "Payment";
+DELETE FROM "RefreshToken";
+DELETE FROM "Subscription";
+DELETE FROM "User";
+DELETE FROM "WhatsappConnectSession";
