636db3ca968bf6471cfa06be66baccf4014dcef3
AuronCrow10 <122154238+AuronCrow10@users.noreply.github.com>
2025-12-07 11:16:57 +0100

    chunk_tables_fix

---
+++ db/init/10-fix-chunk-pages.sql
@@ -0,0 +1,11 @@
+\connect embeddings_db;
+
+-- For SMALL embeddings
+ALTER TABLE page_chunks_small
+  ADD CONSTRAINT page_chunks_small_chunk_hash_unique
+  UNIQUE (client_id, chunk_hash);
+
+-- For LARGE embeddings
+ALTER TABLE page_chunks_large
+  ADD CONSTRAINT page_chunks_large_chunk_hash_unique
+  UNIQUE (client_id, chunk_hash);
